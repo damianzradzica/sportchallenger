0fb6135803ac3097de90aa855c30d4bbf60eac4a
Damian Szymański <damian.szymanski22@gmail.com>
2017-01-28 02:53:03 +0100

    Dynamiczne wyszukiwanie po rodzaju obiektu

--- reservations/static/js/facilities.js
+++ reservations/static/js/facilities.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
     var $search_city = $('.search-city');
     var $search_price_from = $('.search-price-from');
     var $search_price_to= $('.search-price-to');
-    var $search_kind = $('.search-kind').find('input:checkbox:checked');
+    var $search_kind = $('.kind-check');
 
     $search_name.keyup(function(){
         var valThis = $(this).val();
@@ -26,11 +26,36 @@ $(document).ready(function() {
         var valThis = $(this).val();
         $city.each(function(){
             var text = $(this).text().toLowerCase();
-            var element = $(this).parent().parent().parent().parent();
+            var element = $(this).parent().parent();
             (text.indexOf(valThis.toLowerCase()) > -1) ? element.show() : element.hide();
         });
     });
 
+
+
+    $search_kind.on('click', function(){
+        var selected = '';
+        $('.search-kind input:checked').each(function() {
+            selected += $(this).val();
+        });
+        var valThis = selected.replace(/\s+/g, '');
+        $kind.each(function(){
+            var text = $(this).text().toLowerCase().replace('rodzaj obiektu:','').replace(/\s+/g, '');
+            console.log(valThis);
+            console.log(text);
+            var element = $(this).parent().parent();
+            (valThis.toLowerCase().includes(text) == true) ? element.show() : element.hide();
+        });
+    });
+
+
+//    var check_elements = [];
+//    check_elements.push($('.kind-check'));
+//    for(i=0; i<check_elements.length; i++) {
+//        if (check_elements[i].is(':checked')) {
+//            console.log(check_elements[i].text());
+//        }
+//    };
 
 //    console.log($name.text());
 //    console.log($city.text());
