65854f4bd0d1118c0f9fd46fb5ff32ea4fbbeb01
Damian Szymanski <damian.szymanski22@gmail.com>
2017-04-28 00:05:12 +0200

    zmiana bazy danych

--- reservations/reservations/settings.py
+++ reservations/reservations/settings.py
@@ -78,17 +78,10 @@ WSGI_APPLICATION = 'reservations.wsgi.application'
 
 DATABASES = {
     'default': {
-        'HOST': '127.0.0.1',
-        'NAME': 'sport_reservations',
-        'ENGINE': 'mysql.connector.django',
-        'USER': 'root',
-        'PASSWORD': 'coderslab',
-        'OPTIONS': {
-                    'autocommit': True,
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
